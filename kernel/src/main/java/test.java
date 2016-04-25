feca99bcc37cccd4614a33772732ec62fc91ffe3
xufei <manbu.feng@163.com>
2016-04-25 13:36:18 +0800

    test

---
+++ kernel/src/main/java/test.java
@@ -0,0 +1,17 @@
+/**
+ * @author oaoCoder-徐飞
+ * @version V1.0
+ * @Title: test
+ * @Package PACKAGE_NAME
+ * @Description: @TODO
+ * Copyright: Copyright (c) 2015
+ * Company:上海追月信息科技有限公司
+ * @date 2016/4/25 13:28
+ */
+public class test {
+
+    public void test(){
+        System.out.println("hello is a world!");
+    }
+
+}
